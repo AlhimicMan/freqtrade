2dcac5bbfe3ddb7989b2c781c6cfd2a902b3a307
borispahomov <borishome1287@mail.ru>
2021-07-12 12:07:18 +0300

    Add field pairlocl_timeframe to config for setting Pairlock lock interval separated from FT timeframe settings. If parameter not set use FT timeframe setting for pairlock

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -67,7 +67,10 @@ class FreqtradeBot(LoggingMixin):
 
         self.wallets = Wallets(self.config, self.exchange)
 
-        PairLocks.timeframe = self.config['timeframe']
+        pairlocl_timeframe = self.config.get("lock_timeframe")
+        if pairlocl_timeframe is None:
+            pairlocl_timeframe = self.config['timeframe']
+        PairLocks.timeframe = pairlocl_timeframe
 
         self.protections = ProtectionManager(self.config, self.strategy.protections)
 
